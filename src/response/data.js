496ac032e6aee655a80516cd8a39602683db72e3
zabkwak <lukas.macuda@gmail.com>
2022-03-02 15:45:09 +0100

    fixed _data redefinition

--- src/response/data.js
+++ src/response/data.js
@@ -2,14 +2,19 @@ import Base from './base';
 
 export default class DataResponse extends Base {
 
-    constructor(statusCode, data, meta) {
-        super(statusCode, data, meta);
-        if (this._data) {
-            Object.keys(this._data).forEach(key => Object.defineProperty(this, key, { value: this._data[key], enumerable: true }));
-        }
-    }
+	constructor(statusCode, data, meta) {
+		super(statusCode, data, meta);
+		if (this._data) {
+			Object.keys(this._data).forEach((key) => {
+				if (key === '_data') {
+					return;
+				}
+				Object.defineProperty(this, key, { value: this._data[key], enumerable: true });
+			});
+		}
+	}
 
-    isEmpty() {
-        return this._data === null;
-    }
+	isEmpty() {
+		return this._data === null;
+	}
 }
